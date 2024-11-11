8b3419e15d4205dfeb93a55d1e644a74d0cce472
Parisa Saghafian <71983551+Parisistan@users.noreply.github.com>
2024-11-11 16:29:35 +0000

    ARGG-1343: add speaker mute icon (#94)
    Co-authored-by: Parisa Saghafian <parisasaghafian@skyscanner.net>

---
+++ dist/js/icons/sm/speaker-mute.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import type { SVGProps } from 'react';
+declare const SpeakerMuteIcon: (props: SVGProps<SVGSVGElement>) => React.JSX.Element;
+export default SpeakerMuteIcon;
