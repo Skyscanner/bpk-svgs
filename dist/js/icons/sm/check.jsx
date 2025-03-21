54a812270dbcc1b9301a76eeeb9ca66374ff74d5
senzg <80232926+senzg@users.noreply.github.com>
2025-03-21 14:52:18 +0800

    PANGOO-2905/checkbox (#116)
    Co-authored-by: Steven-Zeng <Steven.Zeng@skyscanner.net>

---
+++ dist/js/icons/sm/check.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+const CheckIcon = (props) => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" width="1rem" height="1rem" {...props}>
+    <path stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M2.354 3.646 5.5 7.5l6-6"/>
+  </svg>);
+export default CheckIcon;
