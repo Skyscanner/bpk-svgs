4dadb7d999af0e59bf9c8d3035a812fce9c7276a
IrinaWei <irina.wei@skyscanner.net>
2025-11-20 11:31:41 +0800

    [CLOV-954]add icons for baggages (#171)
    Co-authored-by: Kerrie Wu <kerrie.wu@skyscanner.net>
    Added six new icons:
    - baggage-cabin-included-tick
    - baggage-cabin-uncertain
    - baggage-cabin-not-included-cross
    - baggage-checked-included-tick
    - baggage-checked-uncertain
    - baggage-checked-not-included-cross

---
+++ dist/js/icons/lg/baggage-checked-included-tick.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import type { SVGProps } from 'react';
+declare const BaggageCheckedIncludedTickIcon: (props: SVGProps<SVGSVGElement>) => React.JSX.Element;
+export default BaggageCheckedIncludedTickIcon;
