f8151b1d7112cc63b1cd165bcf25ee4708e151bb
Irina-Wei <irina.wei@skyscanner.net>
2025-11-20 10:33:48 +0800

    add icons

---
+++ dist/js/icons/lg/baggage-cabin-included-tick.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import type { SVGProps } from 'react';
+declare const BaggageCabinIncludedTickIcon: (props: SVGProps<SVGSVGElement>) => React.JSX.Element;
+export default BaggageCabinIncludedTickIcon;
