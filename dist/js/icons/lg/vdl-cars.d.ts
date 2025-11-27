f711716b15d80ec9b1a10fc2b3a1d93f7b2563d8
Kynan Song <kynan.song@skyscanner.net>
2025-11-27 15:30:35 +0000

    [DON-2286] Adding large svgs for recent searches, vertical buttons and tab bar buttons.

---
+++ dist/js/icons/lg/vdl-cars.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import type { SVGProps } from 'react';
+declare const VdlCarsIcon: (props: SVGProps<SVGSVGElement>) => React.JSX.Element;
+export default VdlCarsIcon;
---
+++ dist/js/icons/lg/vdl-tab-drops-selected.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import type { SVGProps } from 'react';
+declare const VdlTabDropsSelectedIcon: (props: SVGProps<SVGSVGElement>) => React.JSX.Element;
+export default VdlTabDropsSelectedIcon;
