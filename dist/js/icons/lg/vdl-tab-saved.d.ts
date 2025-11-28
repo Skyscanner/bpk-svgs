1c21531f75553bb54b57fcef47c7482e7cd31110
Kynan Song <kynan.song@skyscanner.net>
2025-11-28 11:28:59 +0000

    Removing tab and drops icons.

--- dist/js/icons/lg/vdl-tab-saved.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import type { SVGProps } from 'react';
-declare const VdlTabSavedIcon: (props: SVGProps<SVGSVGElement>) => React.JSX.Element;
-export default VdlTabSavedIcon;
