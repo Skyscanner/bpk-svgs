fcf5502e330094e4eda19cf81e173330c1b4c169
Kynan Song <kynan.song@skyscanner.net>
2026-06-17 10:26:21 +0100

    Replacing icons.

--- dist/js/icons/lg/Checkbox-minus.jsx
+++ dist/js/icons/lg/Checkbox-minus.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 const CheckboxMinusIcon = (props) => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" width="1.5rem" height="1.5rem" {...props}>
-    <rect x={13.333} y={21.333} rx={4}/>
+    <rect x={5} y={8} rx={1.5}/>
   </svg>);
 export default CheckboxMinusIcon;
