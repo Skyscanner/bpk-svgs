283571531737451baa48ee54c5b05ff9a85828dc
Kynan Song <kynan.song@skyscanner.net>
2026-06-17 12:37:15 +0100

    Trying old icon.

--- dist/js/icons/lg/Checkbox-minus.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-const CheckboxMinusIcon = (props) => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" width="1.5rem" height="1.5rem" {...props}>
-    <path stroke="#fff" strokeLinecap="round" strokeWidth={8} d="M17.333 26h19"/>
-  </svg>);
-export default CheckboxMinusIcon;
