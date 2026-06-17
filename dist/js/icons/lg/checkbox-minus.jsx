33b4fc84cc15e5bc1aa1da9ae2a238b66fabaf33
Kynan Song <kynan.song@skyscanner.net>
2026-06-17 15:11:53 +0100

    Trying fixed minus icon.

---
+++ dist/js/icons/lg/checkbox-minus.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+const CheckboxMinusIcon = (props) => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" width="1.5rem" height="1.5rem" {...props}>
+    <path stroke="#fff" strokeLinecap="round" strokeWidth={3.6} d="M7.7 12h8.4"/>
+  </svg>);
+export default CheckboxMinusIcon;
