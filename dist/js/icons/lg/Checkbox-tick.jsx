606cc164ec2d3c548c7eb069e3b0b25bcccc3ad5
Kynan Song <kynan.song@skyscanner.net>
2026-06-17 15:54:21 +0100

    Uploading new icons.

--- dist/js/icons/lg/Checkbox-tick.jsx
+++ dist/js/icons/lg/Checkbox-tick.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 const CheckboxTickIcon = (props) => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" width="1.5rem" height="1.5rem" {...props}>
-    <path stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M5.354 9.646 8.5 13.5l6-6"/>
+    <path d="M7.69 13.85a1.8 1.8 0 0 1-2.546-2.546l2.329-2.329a1.8 1.8 0 0 1 2.546 0l.881.881 5.42-5.42a1.8 1.8 0 0 1 2.546 2.546l-6.693 6.693a1.8 1.8 0 0 1-2.546 0L7.69 11.738l-.707.707a1.8 1.8 0 0 1-1.273.527z"/>
   </svg>);
 export default CheckboxTickIcon;
--- dist/js/icons/lg/checkbox-minus.jsx
+++ dist/js/icons/lg/checkbox-minus.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 const CheckboxMinusIcon = (props) => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" width="1.5rem" height="1.5rem" {...props}>
-    <path stroke="#fff" strokeLinecap="round" strokeWidth={3.6} d="M7.7 12h8.4"/>
+    <path d="M7.7 10.2h8.4a1.8 1.8 0 0 1 0 3.6H7.7a1.8 1.8 0 0 1 0-3.6"/>
   </svg>);
 export default CheckboxMinusIcon;
