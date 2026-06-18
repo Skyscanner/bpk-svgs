54da35f0448f3d90ff892080d9ea37e2749d17d8
kynansongwork <36539711+kynansongwork@users.noreply.github.com>
2026-06-18 09:38:58 +0100

    DON-3041 - Adding checkbox icons. (#213)
    Co-authored-by: Kynan Song <kynan.song@skyscanner.net>

---
+++ dist/js/icons/lg/checkbox-minus.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+const CheckboxMinusIcon = (props) => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" width="1.5rem" height="1.5rem" {...props}>
+    <path d="M7.7 10.2h8.4a1.8 1.8 0 0 1 0 3.6H7.7a1.8 1.8 0 0 1 0-3.6"/>
+  </svg>);
+export default CheckboxMinusIcon;
